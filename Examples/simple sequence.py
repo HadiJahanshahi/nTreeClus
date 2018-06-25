ba0901034e6183dd23aafd76f5e8685300edcffc
HadiJahanshahi <36164778+HadiJahanshahi@users.noreply.github.com>
2018-06-25 18:06:08 +0430

    Update simple sequence.py

--- Examples/simple sequence.py
+++ Examples/simple sequence.py
@@ -4,13 +4,14 @@ nTreeClusModel = nTreeClus(my_list, method = "All")
 
 nTreeClusModel
 # {'C_DT': 2,
-# 'C_RF': 2,
-# 'distance_DT': array([ 0.05508882,  0.43305329,  0.68551455,  0.43305329,  0.5       ,
-#         0.7226499 ,  0.5       ,  0.86132495,  0.75      ,  0.4452998 ]),
-# 'distance_RF': array([ 0.0809925 ,  0.56793679,  0.42158647,  0.57878823,  0.56917978,
-#         0.47984351,  0.54545455,  0.55864167,  0.71278652,  0.3341997 ]),
-# 'labels_DT': array([0, 0, 0, 1, 1]),
-# 'labels_RF': array([0, 0, 0, 1, 1])}
+#  'C_RF': 2,
+#  'Parameter n': 4,
+#  'distance_DT': array([ 0.05508882,  0.43305329,  0.68551455,  0.43305329,  0.5       ,
+#          0.7226499 ,  0.5       ,  0.86132495,  0.75      ,  0.4452998 ]),
+#  'distance_RF': array([ 0.0901846 ,  0.51652419,  0.55690357,  0.64739882,  0.5052095 ,
+#          0.66620194,  0.66824925,  0.61354484,  0.76682897,  0.31402302]),
+#  'labels_DT': array([0, 0, 0, 1, 1]),
+#  'labels_RF': array([0, 0, 0, 1, 1])}
 
 
 #It is possible to have the dendogram view of the above example using this code:
@@ -33,4 +34,3 @@ dendrogram(HC_RF_terminal_cosine,labels=my_list, ax=ax)
 ax.tick_params(axis='x', which='major', labelsize=25)
 ax.tick_params(axis='y', which='major', labelsize=15)
 plt.show()
-
